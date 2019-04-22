846a0d8505915066fdaee132b66fcd8c36b1a7a4
lidaimor <lidaimor@163.com>
2019-04-22 09:34:01 +0800

    4-22

---
+++ src/main/java/com/jsp/studyShare/StudyShareApplication.java
@@ -0,0 +1,33 @@
+package com.jsp.studyShare;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.web.servlet.MultipartConfigFactory;
+import org.springframework.context.annotation.Bean;
+
+import javax.servlet.MultipartConfigElement;
+
+@SpringBootApplication()
+@MapperScan("com.jsp.studyShare.Dao")
+public class StudyShareApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudyShareApplication.class, args);
+	}
+
+	/**
+	 * 配置上传文件大小
+	 * @return
+	 */
+	@Bean
+	 public MultipartConfigElement multipartConfigElement() {
+		 MultipartConfigFactory factory = new MultipartConfigFactory();
+		 //单个数据大小
+		 factory.setMaxFileSize("102400KB");
+		 factory.setMaxRequestSize("102400KB");
+		 return factory.createMultipartConfig();
+	 }
+}
